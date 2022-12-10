1d7baf66e64812c8e24c2f3cb0aff131f4e4ec82
safinaabk <safinabulan@gmail.com>
2022-12-10 07:45:58 +0700

    rapiin desain

--- src/App.js
+++ src/App.js
@@ -19,20 +19,10 @@ function App() {
                 About Us
               </a>
             </li>
-            <li>
-              <a href="https://wa.me/6285162525699?text=Halo%20selamat%20pagi,%20saya%20ingin%20reservasi" target="_blank">
-                reservasi
-              </a>
-            </li>
           </ul>
         </nav>
       </header>
       <main>
-        {/* <div className="jumbotron">
-          <img src="/images/jumbotron.png" alt="" width="100%" />
-          <h1 className="jumbo_title">WELCOME TO FIND CAFE</h1>
-          <h2 className="jumbo_desc">Disini Kami menyediakan berbagai macam tempat cafe terbaik yang ada di Kota Bandung</h2>
-        </div> */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/cafes/:id" element={<DetailPage />} />
--- src/components/CafeAppJumbotron.js
+++ src/components/CafeAppJumbotron.js
@@ -3,11 +3,25 @@ import React from 'react';
 function CafeAppJumbotron() {
   return (
     <div className="jumbotron">
-      <img src="/images/jumbotron.png" alt="" width="100%" />
-      <h1 className="jumbo_title">WELCOME TO FIND CAFE</h1>
-      {/* <h2 className="jumbo_desc">
-        more <a href="#list_cafe"></a>
-      </h2> */}
+      <div className="row">
+        <div className="column-header">
+          <img
+            src="/images/bgimg.png"
+            alt=""
+            width="80%
+          "
+          />
+        </div>
+        <div className="column-header">
+          <article className="jumbo_title">
+            <h2 id="judul">WELCOME TO FIND CAFE</h2>
+            <p id="isi">
+              Salah satu website yang menampilkan rekomendasi dan informasi berbagai coffeshop di Kota Bandung. Selain itu, anda dapat langsung melakukan reservasi terhadap coffeshop yang telah anda pilih dan menghubungkan anda langsung
+              dengan coffeshop tersebut{' '}
+            </p>
+          </article>
+        </div>
+      </div>
     </div>
   );
 }
